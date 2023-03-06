c81af6729e4b3c7513834e7ac8e3db1b91fc5e58
AUGUSTINE SMITH <flash@Flashs-MacBook-Pro.local>
2023-03-06 22:37:36 +0000

    lint fixes

---
+++ createdb/management/commands/_lib.py
@@ -0,0 +1,45 @@
+from typing import Mapping
+
+import psycopg2
+import sqlite3
+
+
+class BaseDBCreator:
+    def __init__(self, config: Mapping):
+        self.config = config
+
+    def create(self):
+        raise NotImplementedError("this method must be implemented in a child class")
+
+
+class Sqlite3DBCreator(BaseDBCreator):
+    def create(self):
+        db_name: str = self.config["NAME"]
+        conn = sqlite3.connect(db_name)
+        conn.close()
+
+
+class PostgreSQLDBCreator(BaseDBCreator):
+    def create(self):
+        name = self.config["NAME"]
+        host = self.config["HOST"]
+        user = self.config["USER"]
+        password = self.config["PASSWORD"]
+        port = self.config["PORT"]
+
+        conn = psycopg2.connect(
+            dbname="postgres",
+            host=host,
+            user=user,
+            password=password,
+            port=port,
+        )
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute('CREATE DATABASE "%s"', (name,))
+        conn.close()
+
+
+class MySQLDBCreator(BaseDBCreator):
+    def create(self):
+        pass
